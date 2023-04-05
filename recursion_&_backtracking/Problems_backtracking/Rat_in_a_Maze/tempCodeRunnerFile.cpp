a5322d34acc347e42acbb4a92180dfc8dba1cb85
Shreyas <shreyas.mohanty03@gmail.com>
2023-04-05 15:42:05 +0530

    rat in a maze added

---
+++ recursion_&_backtracking/Problems_backtracking/Rat_in_a_Maze/tempCodeRunnerFile.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<vector<int>> m = {
+        {1, 0, 0, 0},
+        {1, 1, 0, 1},
+        {1, 1, 0, 0},
+        {0, 1, 1, 1};
+};
+
+vector<vector<int>> visited(4, vector<int>(4, 0));
+
+return 0;
+}
