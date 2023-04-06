aa50ba84e6890ece56e2f8cd4e1d427e9a96b0c9
Shreyas <shreyas.mohanty03@gmail.com>
2023-04-06 11:57:51 +0530

    modified rat in a maze

--- recursion_&_backtracking/Problems_backtracking/Rat_in_a_Maze/solution.cpp
+++ recursion_&_backtracking/Problems_backtracking/Rat_in_a_Maze/solution.cpp
@@ -16,42 +16,45 @@ void MazeSolve(vector<vector<int>> visited, vector<vector<int>> &m, int x, int y
     if (x == n - 1 && y == n - 1)
     {
         ans.push_back(path);
-        visited[x][y] = 1;
+        return;
+    }
+    visited[x][y] = 1;
+    // * DOWN
 
-        // * DOWN
-
-        int newx = x + 1;
-        int newy = y;
-        if (checkPath(newx, newy, n, visited, m))
-        {
-            path.push_back('D');
-            MazeSolve(visited, m, newx, newy, ans, path, 4);
-            path.pop_back();
-        }
-        newx = x;
-        newy = y - 1;
-        if (checkPath(newx, newy, n, visited, m))
-        {
-            path.push_back('L');
-            MazeSolve(visited, m, newx, newy, ans, path, 4);
-            path.pop_back();
-        }
-        newx = x - 1;
-        newy = y;
-        if (checkPath(newx, newy, n, visited, m))
-        {
-            path.push_back('U');
-            MazeSolve(visited, m, newx, newy, ans, path, 4);
-            path.pop_back();
-        }
-        newx = x;
-        newy = y + 1;
-        if (checkPath(newx, newy, n, visited, m))
-        {
-            path.push_back('R');
-            MazeSolve(visited, m, newx, newy, ans, path, 4);
-            path.pop_back();
-        }
+    int newx = x + 1;
+    int newy = y;
+    if (checkPath(newx, newy, n, visited, m))
+    {
+        path.push_back('D');
+        MazeSolve(visited, m, newx, newy, ans, path, 4);
+        path.pop_back();
+    }
+    // * LEFT
+    newx = x;
+    newy = y - 1;
+    if (checkPath(newx, newy, n, visited, m))
+    {
+        path.push_back('L');
+        MazeSolve(visited, m, newx, newy, ans, path, 4);
+        path.pop_back();
+    }
+    // * UP
+    newx = x - 1;
+    newy = y;
+    if (checkPath(newx, newy, n, visited, m))
+    {
+        path.push_back('U');
+        MazeSolve(visited, m, newx, newy, ans, path, 4);
+        path.pop_back();
+    }
+    // * RIGHT
+    newx = x;
+    newy = y + 1;
+    if (checkPath(newx, newy, n, visited, m))
+    {
+        path.push_back('R');
+        MazeSolve(visited, m, newx, newy, ans, path, 4);
+        path.pop_back();
     }
 }
 
